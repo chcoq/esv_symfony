d5b2595f92d8c1d7ac9a2827ba4e7b48f0d99660
lecocq <sgfdr>
2018-03-29 22:17:00 +0200

    mise en place du formulaire

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,10 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Competence;
 
 class DefaultController extends Controller
 {
@@ -20,14 +23,29 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/competence", name="competence")
+     * @Route("/competence", name="competence_form")
      */
-    public function competenceAction(Request $request)
+    public function competenceFormAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('accueil/competence.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $competence = new Competence();
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $competence);
+        // ajout des champs du formulaire
+        $formBuilder->add('titre');
+        $formBuilder->add('designation');
+        $formBuilder->add('valider', SubmitType::class);
+        //on récupère l'objet form
+        $form = $formBuilder->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($competence);
+                $em->flush();
+                $this->addFlash('success', "l'article a bien été ajouté");
+                return $this->redirectToRoute('competence_form');
+            }
+        }
+        return $this->render('accueil/competence.html.twig', ['formCompetence' => $form->createView()]);
     }
 
 }
